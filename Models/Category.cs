4f22b262dd8c8704b4fc3e32bee8c995750bf02a
Nima Poshtiban <poshtibannima@gmail.com>
2021-08-16 18:58:11 +0430

    added validation to Category model

--- Models/Category.cs
+++ Models/Category.cs
@@ -11,8 +11,11 @@ namespace Rocky.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Required]
+        [Range(1,int.MaxValue,ErrorMessage ="The amount of Display order should be greater than zero")]
         public int DisplayOrder { get; set; }
 
     }
