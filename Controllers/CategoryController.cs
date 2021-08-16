083321bf9b9c7b2c7605dd4e502490b8ce74f9b7
Nima Poshtiban <poshtibannima@gmail.com>
2021-08-16 18:31:51 +0430

    added create methods for adding category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -21,5 +21,21 @@ namespace Rocky.Controllers
             IEnumerable<Category> objList = _db.Category;
             return View(objList);
         }
+
+        // GET - Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST - Create
+        // Preventing ForgeryToken
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
