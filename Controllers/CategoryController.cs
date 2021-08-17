aabdffe025b15b7ca06053cf723134e281722660
Nima Poshtiban <poshtibannima@gmail.com>
2021-08-17 11:22:18 +0430

    implemented edit functionallity for category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -38,7 +38,41 @@ namespace Rocky.Controllers
             {
                 _db.Category.Add(obj);
                 _db.SaveChanges();
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
+        // GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - Edit
+        // Preventing ForgeryToken
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            // server side validation
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
             }
             else
             {
